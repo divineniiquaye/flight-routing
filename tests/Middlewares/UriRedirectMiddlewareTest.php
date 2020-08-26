797ed8d29f08e64908d08e1d240427c9cf377c86
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-08-26 08:38:52 +0000

    Fixed PSR-4 autoload standard in uri redirect test file

--- tests/Middlewares/UriRedirectMiddlewareTest.php
+++ tests/Middlewares/UriRedirectMiddlewareTest.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Flight\Routing\Tests;
+namespace Flight\Routing\Tests\Middlewares;
 
 use BiuradPHP\Http\Factory\ServerRequestFactory;
 use BiuradPHP\Http\Response;
