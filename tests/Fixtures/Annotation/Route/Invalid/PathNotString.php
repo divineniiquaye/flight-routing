68a4f094730c6abb720cac1a2ee75a081e148ca4
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-08-31 00:06:01 +0000

    Added tests fixtures for annotated routing

---
+++ tests/Fixtures/Annotation/Route/Invalid/PathNotString.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Flight Routing.
+ *
+ * PHP version 7.1 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flight\Routing\Tests\Fixtures\Annotation\Route\Invalid;
+
+use Flight\Routing\Tests\Fixtures\BlankRequestHandler;
+
+/**
+ * @Route(
+ *   name="home",
+ *   path={"/"},
+ *   methods={"GET"}
+ * )
+ */
+class PathNotString extends BlankRequestHandler
+{
+}
---
+++ tests/Fixtures/Annotation/Route/Valid/PingRequestHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Flight Routing.
+ *
+ * PHP version 7.1 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flight\Routing\Tests\Fixtures\Annotation\Route\Valid;
+
+use Flight\Routing\Tests\Fixtures\BlankRequestHandler;
+
+/**
+ * @Route(
+ *   name="ping",
+ *   path="/ping",
+ *   methods={"HEAD", "GET"},
+ *   middlewares={
+ *     "Sunrise\Http\Router\Tests\Fixture\BlankMiddleware",
+ *     "Sunrise\Http\Router\Tests\Fixture\BlankMiddleware"
+ *   },
+ *   defaults={
+ *     "foo": "bar",
+ *     "bar": "baz"
+ *   }
+ * )
+ */
+class PingRequestHandler extends BlankRequestHandler
+{
+}
