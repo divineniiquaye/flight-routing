2b4267535c83064ba526b735d77a0fadd0c9c204
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-01-28 07:06:58 +0000

    Updated route list class drastically (BR Change)

--- src/RouteList.php
+++ src/RouteList.php
@@ -17,283 +17,332 @@ declare(strict_types=1);
 
 namespace Flight\Routing;
 
-use ArrayIterator;
-use Countable;
-use Flight\Routing\Interfaces\RouteGroupInterface;
-use Flight\Routing\Interfaces\RouteInterface;
-use Flight\Routing\Interfaces\RouteListInterface;
-use IteratorAggregate;
+use Flight\Routing\Exceptions\InvalidControllerException;
 
 /**
  * A RouteCollection represents a set of Route instances.
+ * 
+ * This class provides all(*) methods for creating path+HTTP method-based routes and
+ * injecting them into the router:
  *
- * When adding a route at the end of the collection, an existing route
- * with the same name is removed first. So there can only be one route
- * with a given name.
+ * - head
+ * - get
+ * - post
+ * - put
+ * - patch
+ * - delete
+ * - options
+ * - any
+ * - resource
+ *
+ * A general `addRoute()` method allows specifying multiple request methods and/or
+ * arbitrary request methods when creating a path-based route.
+ *
+ * __call() forwards method-calls to Route, but returns instance of RouteCollection
+ * listing Route's methods below, so that IDEs know they are valid
+ *
+ * @method RouteCollection withAssert(string $variable, string $regexp)
+ * @method RouteCollection withDefault(string $variable, mixed $default)
+ * @method RouteCollection withArgument($variable, mixed $value)
+ * @method RouteCollection withMethod(string ...$methods)
+ * @method RouteCollection withScheme(string ...$schemes)
+ * @method RouteCollection withMiddleware(mixed ...$middlewares)
+ * @method RouteCollection withDomain(string ...$hosts)
+ * @method RouteCollection withPrefix(string $path)
  *
  * @author Fabien Potencier <fabien@symfony.com>
  * @author Tobias Schultze <http://tobion.de>
  * @author Divine Niiquaye Ibok <divineibok@gmail.com>
  */
-final class RouteList implements RouteListInterface, IteratorAggregate, Countable
+final class RouteList extends \ArrayIterator
 {
-    /** @var RouteInterface[] */
-    private $list = [];
+    /** @var null|string */
+    private $namePrefix;
 
-    public function __clone()
+    /** @var Route */
+    private $defaultRoute;
+
+    /**
+     * @param Route|null $defaultRoute
+     * @param mixed $defaultHandler
+     */
+    public function __construct(?Route $defaultRoute = null, $defaultHandler = null)
     {
-        foreach ($this->list as $index => $route) {
-            $this->list[$index] = clone $route;
+        $this->defaultRoute = $defaultRoute ?? new Route('/', '', $defaultHandler);
+    }
+
+    public function __call($method, $arguments)
+    {
+        $routeMethod = \strtolower(\preg_replace('~^with([A-Z]{1}[a-z]+)$~', '\1', $method, 1));
+
+        if (!\method_exists($this->defaultRoute, $routeMethod)) {
+            throw new \BadMethodCallException(
+                \sprintf(
+                    'Method "%s::%s" does not exist. %2$s method should have a \'with\' prefix',
+                    Route::class,
+                    $method
+                )
+            );
         }
+
+        \call_user_func_array([$this->defaultRoute, $routeMethod], $arguments);
+
+        foreach ($this as $route) {
+            \call_user_func_array([$route, $routeMethod], $arguments);
+        }
+
+        return $this;
     }
 
     /**
-     * Gets the current RouteCollection as an Iterator that includes all routes.
+     * Gets the current RouteCollection as an array that includes all routes.
      *
-     * It implements \IteratorAggregate.
+     * Use this method to fetch routes instead of getIterator().
      *
-     * @see all()
-     *
-     * @return ArrayIterator<int,RouteInterface> An \ArrayIterator object for iterating over routes
-     */
-    public function getIterator()
-    {
-        return new ArrayIterator($this->getRoutes());
-    }
-
-    /**
-     * Gets the number of Routes in this collection.
-     *
-     * @return int The number of routes
-     */
-    public function count()
-    {
-        return \iterator_count($this);
-    }
-
-    /**
-     * {@inheritdoc}
+     * @return Route[] The filtered merged routes
      */
     public function getRoutes(): array
     {
-        return $this->list;
+        return $this->doMerge('', new static());
     }
 
     /**
-     * {@inheritdoc}
+     * Add route(s) to the collection
+     *
+     * @param Route ...$routes
+     *
+     * @return self
      */
-    public function add(RouteInterface $route): RouteListInterface
-    {
-        $this->list[] = $route;
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function addCollection(RouteListInterface $collection): void
-    {
-        // we need to remove all routes with the same names first because just replacing them
-        // would not place the new route at the end of the merged array
-        foreach ($collection->getRoutes() as $index => $route) {
-            unset($this->list[$index]);
-
-            $this->list[$index] = $route;
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function addForeach(RouteInterface ...$routes): RouteListInterface
+    public function add(Route ...$routes): self
     {
         foreach ($routes as $route) {
-            $this->add($route);
+            $default = clone $this->defaultRoute;
+
+            if (null === $route->getController()) {
+                $route->run($default->getController());
+            }
+
+            $this[]  = $default::__set_state($route->getAll());
         }
 
         return $this;
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a pattern to a handler.
+     *
+     * You can must specify HTTP methods that should be matched.
+     *
+     * @param string   $pattern Matched route pattern
+     * @param string[] $methods Matched HTTP methods
+     * @param mixed    $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function addRoute(string $name, array $methods, string $pattern, $handler): RouteInterface
+    public function addRoute(string $pattern, array $methods, $handler = null): Route
     {
-        $this->add($route = new Route($name, $methods, $pattern, $handler));
+        $route      = clone $this->defaultRoute;
+        $controller = null === $handler ? $route->getController() : $handler;
+        
+        $route->path($pattern)->method(...$methods);
+
+        $this[] = $route;
+        $route->run($controller);
 
         return $route;
     }
 
     /**
-     * {@inheritdoc}
+     * Mounts controllers under the given route prefix.
+     *
+     * @param string                   $named       the route group prefixed name
+     * @param callable|RouteCollection $controllers A RouteCollection instance or a callable for defining routes
+     *
+     * @throws LogicException
      */
-    public function group($callable): RouteGroupInterface
+    public function group(string $name, $controllers): self
     {
-        $collector = new static();
-        $callable($collector);
+        if (\is_callable($controllers)) {
+            $collection = new static();
+            \call_user_func($controllers, $collection);
+            $controllers = clone $collection;
+        } elseif (!$controllers instanceof self) {
+            throw new \LogicException('The "mount" method takes either a "RouteCollection" instance or callable.');
+        }
 
-        $this->addForeach(...$routes = $collector->getRoutes());
+        $controllers->namePrefix = $name;
 
-        return new RouteGroup($routes);
+        $this[] = $controllers;
+
+        return $controllers;
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a HEAD request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function head(string $name, string $pattern, $callable): RouteInterface
+    public function head(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, [Route::METHOD_HEAD], $pattern, $callable);
+        return $this->addRoute($pattern, [Router::METHOD_HEAD], $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a GET and HEAD request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function get(string $name, string $pattern, $callable): RouteInterface
+    public function get(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, [Route::METHOD_GET, Route::METHOD_HEAD], $pattern, $callable);
+        return $this->addRoute($pattern, [Router::METHOD_GET, Router::METHOD_HEAD], $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a POST request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function post(string $name, string $pattern, $callable): RouteInterface
+    public function post(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, [Route::METHOD_POST], $pattern, $callable);
+        return $this->addRoute($pattern, [Router::METHOD_POST], $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a PUT request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function put(string $name, string $pattern, $callable): RouteInterface
+    public function put(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, [Route::METHOD_PUT], $pattern, $callable);
+        return $this->addRoute($pattern, [Router::METHOD_PUT], $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a PATCH request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function patch(string $name, string $pattern, $callable): RouteInterface
+    public function patch(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, [Route::METHOD_PATCH], $pattern, $callable);
+        return $this->addRoute($pattern, [Router::METHOD_PATCH], $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a DELETE request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function delete(string $name, string $pattern, $callable): RouteInterface
+    public function delete(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, [Route::METHOD_DELETE], $pattern, $callable);
+        return $this->addRoute($pattern, [Router::METHOD_DELETE], $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps a OPTIONS request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function options(string $name, string $pattern, $callable): RouteInterface
+    public function options(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, [Route::METHOD_OPTIONS], $pattern, $callable);
+        return $this->addRoute($pattern, [Router::METHOD_OPTIONS], $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps any request to a handler.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $handler Handler that returns the response when matched
+     *
+     * @return Route
      */
-    public function any(string $name, string $pattern, $callable): RouteInterface
+    public function any(string $pattern, $handler = null): Route
     {
-        return $this->addRoute($name, Route::HTTP_METHODS_STANDARD, $pattern, $callable);
+        return $this->addRoute($pattern, Router::HTTP_METHODS_STANDARD, $handler);
     }
 
     /**
-     * {@inheritdoc}
+     * Maps any request to a resource handler and prefix class method by request method.
+     * If you request on "/account" path with a GET method, prefixed by the name
+     * parameter eg: 'user', class method will match `getUser`.
+     *
+     * @param string              $name     The prefixed name attached to request method
+     * @param string              $pattern  matched path where request should be sent to
+     * @param class-string|object $resource Handler that returns the response
+     *
+     * @return Route
      */
-    public function resource(string $name, string $pattern, $resource): RouteInterface
+    public function resource(string $name, string $pattern, $resource): Route
     {
-        if (\is_callable($resource)) {
-            throw new Exceptions\InvalidControllerException(
-                'Resource handler type should be a string or object class, but not a callable'
+        if (!is_object($resource) || (is_string($resource) && class_exists($resource))) {
+            throw new InvalidControllerException(
+                'Resource handler type should be a class string or class object, but not a callable or array'
             );
         }
 
-        return $this->any($name . '__restful', $pattern, [$resource, $name]);
+        $route = $this->any($pattern, [$resource, $name]);
+
+        return $route->bind($route->generateRouteName($name) . '__restful');
     }
 
     /**
-     * {@inheritdoc}
+     * Find a route by name.
+     *
+     * @param string $name The route name
+     *
+     * @return null|Route A Route instance or null when not found
      */
-    public function withDefaults(array $defaults): void
+    public function find(string $name): ?Route
     {
-
-        foreach ($this->list as $route) {
-            $route->setDefaults($defaults);
+        foreach ($this as $route) {
+            if ($name === $route->getName()) {
+                return $route;
+            }
         }
+
+        return null;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function withName(string $name): void
+    private function doMerge(string $prefix, self $routes)
     {
-        foreach ($this->list as $route) {
-            $route->setName($name . $route->getName());
-        }
-    }
+        /** @var Route|RouteCollection $route */
+        foreach ($this as $route) {
+            if ($route instanceof Route) {
+                if (null === $name = $route->getName()) {
+                    $name = $base = $route->generateRouteName('');
+                    $i    = 0;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function withPrefix(string $prefix): void
-    {
-        foreach ($this->list as $route) {
-            $route->addPrefix($prefix);
-        }
-    }
+                    while ($routes->find($name)) {
+                        $name = $base . '_' . ++$i;
+                    }
+                }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function withDomain(string $domain): void
-    {
-        foreach ($this->list as $route) {
-            $route->setDomain($domain);
+                $routes[] = $route->bind($prefix . $name);
+            } else {
+                $route->doMerge($prefix . $route->namePrefix, $routes);
+            }
         }
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function withScheme(string ...$schemes): void
-    {
-        foreach ($this->list as $route) {
-            $route->setScheme(...$schemes);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function withMethod(string ...$methods): void
-    {
-        foreach ($this->list as $route) {
-            $route->addMethod(...$methods);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function withMiddleware(...$middlewares): void
-    {
-        foreach ($this->list as $route) {
-            $route->addMiddleware(...$middlewares);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function withPatterns(array $patterns): void
-    {
-        foreach ($this->list as $route) {
-            $route->setPatterns($patterns);
-        }
+        return $routes->getArrayCopy();
     }
 }
