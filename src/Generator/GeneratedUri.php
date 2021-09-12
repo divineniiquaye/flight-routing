3e43986e49c0215458e8e05cda2b82e7129bfc23
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-09-12 08:33:39 +0000

    Moved GeneratedUri class under Generator sub-namespace

--- src/Generator/GeneratedUri.php
+++ src/Generator/GeneratedUri.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Flight\Routing;
+namespace Flight\Routing\Generator;
 
 /**
  * A generated URI from route made up of only the
