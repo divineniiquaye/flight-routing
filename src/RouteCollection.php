e5241d0cacb7fabfa7895bdf01056214e7732877
Divine Niiquaye Ibok <divineibok@gmail.com>
2022-05-07 20:04:36 +0000

    Updated the route collection class with merged group traits method

--- src/RouteCollection.php
+++ src/RouteCollection.php
@@ -41,13 +41,17 @@ namespace Flight\Routing;
 class RouteCollection
 {
     use Traits\PrototypeTrait;
-    use Traits\GroupingTrait;
+
+    private ?string $namedPrefix;
+    private ?Route $route = null;
+    private ?self $parent = null;
+    private bool $sortRoutes, $locked = false;
 
     /** @var array<int,Route> */
     private array $routes = [];
 
-    private ?Route $route = null;
-    private bool $locked = false;
+    /** @var array<int,self> */
+    private array $groups = [];
 
     /**
      * @param string $namedPrefix The unqiue name for this group
@@ -172,6 +176,49 @@ class RouteCollection
         }
 
         return $this;
+    }
+
+    /**
+     * Mounts controllers under the given route prefix.
+     *
+     * @param string|null                   $name        The route group prefixed name
+     * @param callable|RouteCollection|null $controllers A RouteCollection instance or a callable for defining routes
+     *
+     * @throws \TypeError        if $controllers not instance of route collection's class
+     * @throws \RuntimeException if locked
+     *
+     * @return $this
+     */
+    public function group(string $name = null, $controllers = null)
+    {
+        if (\is_callable($controllers)) {
+            $controllers($routes = $this->injectGroup($name, new static($name)));
+        }
+
+        return $this->groups[] = $routes ?? $this->injectGroup($name, $controllers ?? new static($name));
+    }
+
+    /**
+     * Merge a collection into base.
+     *
+     * @throws \RuntimeException if locked
+     */
+    public function populate(self $collection, bool $asGroup = false): void
+    {
+        if ($asGroup) {
+            $this->groups[] = $this->injectGroup($collection->namedPrefix, $collection);
+        } else {
+            $this->includeRoute($collection); // Incase of missing end method call on route.
+            $routes = $collection->routes;
+
+            if (!empty($collection->groups)) {
+                $collection->injectGroups($collection->namedPrefix ?? '', $routes);
+            }
+
+            foreach ($routes as $route) {
+                $this->routes[] = $this->injectRoute($route);
+            }
+        }
     }
 
     /**
@@ -351,5 +398,59 @@ class RouteCollection
             $collection->prototypes = \array_merge($this->prototypes, $collection->prototypes);
             $collection->parent = $this;
         }
+    }
+
+    /**
+     * @return self|$this
+     */
+    protected function injectGroup(?string $prefix, self $controllers)
+    {
+        if ($this->locked) {
+            throw new \RuntimeException('Cannot add a nested routes collection to a frozen routes collection.');
+        }
+
+        if ($controllers->sortRoutes) {
+            throw new \RuntimeException('Cannot sort routes in a nested collection.');
+        }
+
+        $this->includeRoute($controllers); // Incase of missing end method call on route.
+
+        if (empty($controllers->namedPrefix)) {
+            $controllers->namedPrefix = $prefix;
+        }
+
+        return $controllers;
+    }
+
+    /**
+     * @param array<int,Route> $collection
+     */
+    private function injectGroups(string $prefix, array &$collection): void
+    {
+        $unnamedRoutes = [];
+
+        foreach ($this->groups as $group) {
+            $group->includeRoute(); // Incase of missing end method call on route.
+
+            foreach ($group->routes as $route) {
+                if (empty($name = $route->getName())) {
+                    $name = $route->generateRouteName('');
+
+                    if (isset($unnamedRoutes[$name])) {
+                        $name .= ('_' !== $name[-1] ? '_' : '') . ++$unnamedRoutes[$name];
+                    } else {
+                        $unnamedRoutes[$name] = 0;
+                    }
+                }
+
+                $collection[] = $route->bind($prefix . $group->namedPrefix . $name);
+            }
+
+            if (!empty($group->groups)) {
+                $group->injectGroups($prefix . $group->namedPrefix, $collection);
+            }
+        }
+
+        $this->groups = [];
     }
 }
